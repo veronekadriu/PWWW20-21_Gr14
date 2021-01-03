0ce810ae3c13af5d6417352b13431f9b19332548
veronekadriu <verone.kadriu@studentet.uni-pr.edu>
2021-01-03 18:35:04 +0100

    added game

---
+++ js/game.js
@@ -0,0 +1,61 @@
+var ball;
+
+function startGame() {
+    ball = new component(20, 20, "#156694", 230, 120);
+    area.start();
+}
+
+var area = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.id = 'game';
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[6]);
+        this.interval = setInterval(updateGameArea, 10);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ct = area.context;
+        ct.fillStyle = color;
+        ct.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
+
+function updateGameArea() {
+    area.clear();
+    ball.newPos();    
+    ball.update();
+}
+
+function moveup() {
+    ball.speedY -= 1; 
+}
+
+function movedown() {
+    ball.speedY += 1; 
+}
+
+function moveleft() {
+    ball.speedX -= 1; 
+}
+
+function moveright() {
+    ball.speedX += 1; 
+}
