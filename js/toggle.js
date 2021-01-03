3888be7ca6826dc1839cd68741c9a949d25f87ef
veronekadriu <verone.kadriu@studentet.uni-pr.edu>
2021-01-03 23:31:01 +0100

    fixed toggle

--- js/toggle.js
+++ js/toggle.js
@@ -1,18 +1,17 @@
-// start by getting all the questions and answers
-// these will be put into arrays
+
 
 var questions = document.getElementsByTagName('dt');
 var answers = document.getElementsByTagName('dd');
 
 
-// function for the link that turns them all off
+
 function toggleAllOff(){
 	for (var i = 0; i < answers.length; i++) { 
 		answers[i].className = 'hide';
 	}
 }
 
-// function for the link that turns them all on
+
 function toggleAllOn(){
 	for (var i = 0; i < answers.length; i++) { 
 		answers[i].className = 'show';
@@ -28,21 +27,20 @@ function toggleNext(el) {
 }
 
 
-//makes the definition lists click-able
 function displayToggle(){
 	
-	toggleAllOff(); // calls the toggle all off function to turn all the answers off when the page is loaded	
+	toggleAllOff(); 
 	 
-	 for (i=0; i<questions.length; i++) { // loops through the questions a
-		 questions[i].onclick=function() { // shows the answers onclick
+	 for (i=0; i<questions.length; i++) { 
+		 questions[i].onclick=function() { 
 		 	var next = this.nextSibling;
-			while(next.nodeType != 1) next=next.nextSibling; // if it gets to a non-element node, go to the next one
+			while(next.nodeType != 1) next=next.nextSibling; 
 			next.className=((next.className=="hide") ? "show" : "hide");
 		}
 	 }
 }
 
-// initiates the click-able dt's when the page loads
+
 window.onload=function() {
 	displayToggle();
 	}
